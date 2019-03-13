02fb4b0ad0f7393d9b07df4b52db321e4198d503
george101010 <47598032+george101010@users.noreply.github.com>
2019-03-13 16:05:24 +0300

    .

---
+++ lab 1/complex_metods.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<math>
+ 
+#include"complex_number.h"
+using namespace std;
+void arifm(compnum *a){
+     cout<<pow(a.re*a.re+a.im*a.im,0.5);
+     }
+void arifm(string tk, compnum *a, compnum *b){
+     switch (tk)
+     case "+" :
+                {   
+                cout<<a.re+b.re<<"+"<<a.im+b.im<<"i";
+                break;
+                }
+     case "-" : {cout<<a.re-b.re<<"+"<<a.im-b.im<<"i";
+                break;}
+     case "*" : {cout<<a.re*b.re-a.im*b.im<<"+"<<a.re*b.im+b.re*a.im<<"i";
+                break;}
+     case "/" :{if (b.im==0) and (b.re==0) {
+                    cout<<"Devision by zero";}
+                 else{
+                 cout<<(a.re*b.re+a.im*b.im)/(b.im*b.im+b.re*b.re)<<"+"<<(b.re*a.im-a.re*b.im)/(b.im*b.im+b.re*b.re)<<"i";}
+                break;}
+     default:{
+              cout<<"syntax error aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa blyad";
+                break;} }
---
+++ lab 1/complex_number.h
@@ -0,0 +1,8 @@
+#ifndef COMPLEX_NUMBER
+#define COMPLEX_NUMBER
+struct compnum{
+       float re,im;      
+       };
+void arifm(compnum *a);
+void arifm(string tk, compnum *a, compnum *b);
+#endef
