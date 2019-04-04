c6edc11ec122563e842fa836ac02efaad02967da
george101010 <47598032+george101010@users.noreply.github.com>
2019-04-04 23:16:22 +0300

    /

---
+++ lab 3/lb 3 1.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<string>
+// создание класса child
+using namespace std;
+class child{
+    private:
+        string name;
+        string surname;
+        int age;
+    public://сетеры класса
+        void setage(int c);
+        void setname(string s);
+        void setsurname(string s);
+        //гетеры класса
+        int getage();
+        string getname();
+        string getsurname();
+        };
+void child::setage(int c){
+    age=c;}
+void child::setname(string s){
+    name=s;}
+void child::setsurname(string s){
+    surname=s;}
+
+string child::getname(){
+    return name;}
+
+string child::getsurname(){
+    return surname;}
+
+int child::getage(){
+    return age;}
+int main(){ //основная программа ввод и вывод данных 2 объектов класса child
+    string sur,nm;
+    int ag;
+
+    child ch1;
+    cout<<"Enter surname, name and age of child N1 through the gap: ";
+    cin>>sur>>nm>>ag;
+    ch1.setage(ag);
+    ch1.setname(nm);
+    ch1.setsurname(sur);
+    cout<<"Child N1:"<<ch1.getsurname()<<" "<<ch1.getname()<<" "<< ch1.getage()<<endl;
+    child ch2;
+    cout<<"Enter surname, name and age of child N2 through the gap: ";
+    cin>>sur>>nm>>ag;
+    ch2.setage(ag);
+    ch2.setname(nm);
+    ch2.setsurname(sur);
+    cout<<"Child N2:"<<ch2.getsurname()<<" "<<ch2.getname()<<" "<< ch2.getage()<<endl;
+    return 0;}
---
+++ lab 3/lb 3 2.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+#include<string>
+// создание класса child
+//поля класса брэнд, длина, ширина, цена
+using namespace std;
+class tiles{
+    private:
+        string brand;
+        int size_h;
+        int size_w;
+        int price;
+    public://сетеры класса
+        void setbrand(string s);
+        void setsizeh(int c);
+        void setsizew(int c);
+        void setprice(int c);
+
+        //гетеры класса
+        void getdata();
+
+        };
+void tiles::setsizew(int c){
+    size_w=c;}
+void tiles::setsizeh(int c){
+    size_h=c;}
+void tiles::setbrand(string s){
+    brand=s;}
+void tiles::setprice(int c){
+    price=c;}
+
+void tiles::getdata(){
+    cout<<brand<<" "<<size_h<<" "<<size_w<<" "<<price<<" ";}
+int main(){ //основная программа ввод и вывод данных объекта класса tiles
+    string br;
+    int h,w,p;
+
+    tiles tl;
+    cout<<"Enter brand, height and wideness and price of tile through the gap: ";
+    cin>>br>>h>>w>>p;
+    tl.setbrand(br);
+    tl.setsizeh(h);
+    tl.setsizew(w);
+    tl.setprice(p);
+    cout<<"Data of this tile(brand, height, wideness, price): ";
+    tl.getdata();
+
+    return 0;}
