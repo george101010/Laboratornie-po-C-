6b49a9dd1bfd2a4dea8e0bfe46f0d31c8311d17f
george101010 <47598032+george101010@users.noreply.github.com>
2019-04-24 16:37:50 +0300

    .

---
+++ lab 5/lb 5 1.cpp
@@ -0,0 +1,98 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+// создание класса комплексное число
+//поля класса действительная часть и мнимая
+using namespace std;
+class complex{
+    private:
+        float re;
+        float im;
+    public://конструктор  класса
+        complex(float r,float i){
+            re=r;
+            im=i;}
+        //деструктор  класса
+        //~complex(){cout<<"Complex number was deleted."<<endl;}
+    friend ostream& operator<< (ostream &out, const complex &complex);// перегрузка cout
+    friend istream& operator>>(istream& cin, complex &complex);// перегрузка cin
+    friend  complex operator++(complex& a,int);//перегрузка инкремента
+    friend  complex operator--(complex& a,int);//перегрузка декремента
+     complex& operator=(const complex& right){//перегрузка присваивания
+    this->re=right.re;
+    this->im=right.im;
+    return *this;}
+    friend bool  operator>(complex& lef,complex& rig);//перегрузка  знака больше
+    friend bool  operator<(complex& lef,complex& rig);//перегрузка  знака меньше
+    friend bool  operator==(complex& lef,complex& rig);//перегрузка  знака =
+};
+
+ostream& operator<< (ostream &out, const complex &a){ // перегрузка cout
+     if (a.re!=0){
+        out<<a.re;}
+     if (a.im==0){
+        return out;
+     }
+     if (a.im>0){out<<"+";}
+    out<<a.im<<"i";
+    return out;}
+
+istream& operator>>(istream& cin, complex &a){  //перегрузка cin
+    cin>>a.re>>a.im;
+    return cin;
+    }
+
+complex  operator++(complex& a,int){//перегрузка  знака ++
+    complex s(a.re++,a.im);
+    return s;}
+
+complex  operator--(complex& a,int){ //перегрузка  знака --
+    complex s(a.re--,a.im);
+    return s;}
+bool operator>(complex& lef,complex& rig){ //перегрузка  знака >
+
+        return pow(lef.re*lef.re+lef.im*lef.im,0.5)>pow(rig.re*rig.re+rig.im*rig.im,0.5);
+        }
+bool operator<(complex& lef,complex& rig){ //перегрузка  знака <
+
+        return pow(lef.re*lef.re+lef.im*lef.im,0.5)<pow(rig.re*rig.re+rig.im*rig.im,0.5);
+        }
+bool operator==(complex& lef,complex& rig){ //перегрузка  знака ==
+
+        return pow(lef.re*lef.re+lef.im*lef.im,0.5)==pow(rig.re*rig.re+rig.im*rig.im,0.5);
+        }
+int main(){ //основная программа ввод и вывод данных объекта класса complex
+
+
+
+    complex q(0,0);
+    cout<<"Enter first complex number: ";
+    cin>>q;
+    complex wer(0,0);
+    cout<<"Enter second complex number: ";
+    cin>>wer;
+    q++;
+    cout<<"Increment: "<<q<<endl;
+    wer--;
+    cout<<"Decrement: "<<wer<<endl;
+    if (q>wer) {
+        cout<<q<<" more than "<<wer<<endl;
+    }
+    else {
+        cout<<wer<<" more than "<<q<<endl;
+    }
+
+    if (q<wer) {
+        cout<<q<<" less than "<<wer<<endl;
+    }
+    else {
+        cout<<wer<<"less than "<<q<<endl;
+    }
+    if (q==wer) {
+        cout<<q<<" equal "<<wer<<endl;
+    }
+    else {
+        cout<<wer<<" not equal "<<q<<endl;
+    }
+    return 0;
+    }
---
+++ lab 5/lb 5 2.cpp
@@ -0,0 +1,87 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+// создание класса вектор 3д
+//поля класса координаты по X,Y,Z
+using namespace std;
+class vect{
+    private:
+        float ox;
+        float oy;
+        float oz;
+    public://конструктор  класса 
+        vect(float X,float Y,float Z){
+            ox=X;
+            oy=Y;
+            oz=Z;}
+        friend ostream& operator<< (ostream &out, const vect &a);// перегрузка cout
+        friend istream& operator>>(istream& cin, vect &vect);// перегрузка cin
+        //сравниваются модули векторов
+        friend bool  operator>(vect& lef,vect& rig);//перегрузка  знака больше
+        friend bool  operator<(vect& lef,vect& rig);//перегрузка  знака меньше
+        friend bool  operator==(vect& lef,vect& rig);//перегрузка  знака =
+        vect& operator=(const vect& right){//перегрузка присваивания
+        this->ox=right.ox;
+        this->oy=right.oy;
+        this->oz=right.oz;
+        return *this;}
+        friend bool  operator>(vect& lef,vect& rig);//перегрузка  знака больше
+        friend bool  operator<(vect& lef,vect& rig);//перегрузка  знака меньше
+        friend bool  operator==(vect& lef,vect& rig);//перегрузка  знака =
+
+
+
+
+        };
+ostream& operator<< (ostream &out, const vect &a){ // перегрузка cout
+
+    out<<a.ox<<" " <<a.oy<<" " <<a.oz;
+    return out;}
+
+istream& operator>>(istream& cin, vect &a){  //перегрузка cin
+    cin>>a.ox>>a.oy>>a.oz;
+    return cin;
+    }
+
+bool operator>(vect& lef,vect& rig){ //перегрузка  знака >
+
+        return pow(lef.ox*lef.ox+lef.oy*lef.oy+lef.oz*lef.oz,0.5)>pow(rig.ox*rig.ox+rig.oy*rig.oy+rig.oz*rig.oz,0.5);
+        }
+bool operator<(vect& lef,vect& rig){ //перегрузка  знака <
+
+        return pow(lef.ox*lef.ox+lef.oy*lef.oy+lef.oz*lef.oz,0.5)<pow(rig.ox*rig.ox+rig.oy*rig.oy+rig.oz*rig.oz,0.5);
+        }
+bool operator==(vect& lef,vect& rig){ //перегрузка  знака ==
+
+        return pow(lef.ox*lef.ox+lef.oy*lef.oy+lef.oz*lef.oz,0.5)==pow(rig.ox*rig.ox+rig.oy*rig.oy+rig.oz*rig.oz,0.5);
+        }
+
+
+int main(){
+    vect q(0,0,0);
+    vect wer(0,0,0);
+    cout<<"Enter coordinates of first vector: ";
+    cin>>q;
+    wer=q;
+    cout<<"Enter coordinates of second vector: ";
+    cin>>q;
+    if (q>wer) {
+        cout<<"Module of "<<q<<" more than module of "<<wer<<endl;
+    }
+    else {
+        cout<<"Module of "<<wer<<" more than module of "<<q<<endl;
+    }
+
+    if (q<wer) {
+        cout<<"Module of "<<q<<" less than module of "<<wer<<endl;
+    }
+    else {
+        cout<<"Module of "<<wer<<" less than module of "<<q<<endl;
+    }
+    if (q==wer) {
+        cout<<"Module of "<<q<<" equal module of "<<wer<<endl;
+    }
+    else {
+        cout<<"Module of "<<wer<<" not equal module of "<<q<<endl;
+    }
+    return 0;}
