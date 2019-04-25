bb1e1628678f0e0193a14719c3972396c5792689
george101010 <47598032+george101010@users.noreply.github.com>
2019-04-25 20:54:50 +0300

    .

--- lab 2/lab 2 1.cpp
+++ lab 2/lab 2 1.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+п»ї#include<iostream>
 using namespace std;
 int mins(int a,int b){
-    return (b-a);}// возвращает разность чисел
+    return (b-a);}// РІРѕР·РІСЂР°С‰Р°РµС‚ СЂР°Р·РЅРѕСЃС‚СЊ С‡РёСЃРµР»
 int umn(int a,int b){
-    return (a*b);// возвращает произведение чисел
+    return (a*b);// РІРѕР·РІСЂР°С‰Р°РµС‚ РїСЂРѕРёР·РІРµРґРµРЅРёРµ С‡РёСЃРµР»
     }
 int main() {
      int n,v;
      cin>>n>>v;
      int (*q)(int a,int b);
-     if (v>n) {q=mins;}//при v>n сработает вычитание
-     else {q=umn;}//иначе сработает умножение
+     if (v>n) {q=mins;}//РїСЂРё v>n СЃСЂР°Р±РѕС‚Р°РµС‚ РІС‹С‡РёС‚Р°РЅРёРµ
+     else {q=umn;}//РёРЅР°С‡Рµ СЃСЂР°Р±РѕС‚Р°РµС‚ СѓРјРЅРѕР¶РµРЅРёРµ
 
      cout<<q(n,v);
      return 0;}
