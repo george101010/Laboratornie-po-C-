bf4fb9ebff463829eb32e0c9a7d6d522bea90eab
george101010 <47598032+george101010@users.noreply.github.com>
2019-04-25 22:33:30 +0300

    /

---
+++ lab 8/lb 8 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    int n=0;
+    vector<string> stud;
+    string data="";
+    cout<<"Enter surname and name of students. "<< endl;
+    cout<<"At the end enter empty string. "<< endl;
+    do {
+        getline(cin, data);
+        if (data.size()>0){
+            n++; //добавление одного элемента в вектор
+            stud.push_back(data);
+        }
+    } while (data != "");
+
+    //вывод заполненного вектора на экран
+    cout<<"Students: "<< endl;
+    for (int i=0;i<n;i++) {
+        cout << stud[i] << endl; }
+    return 0;
+}
---
+++ lab 8/lb 8 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+class complex {//класс complex
+    private:
+    float re,im;
+    public:
+    void constr(float a,float b){//конструктор и гетеры
+        re=a;
+        im=b;
+        }
+
+    float getre(){
+        return re;}
+    float getim(){
+        return im;}
+    };
+using namespace std;
+int main(){
+    complex t;
+    float r,m;
+    int w;
+    cout<<"Enter number of complex numbers: ";
+    cin>>w;
+    list<complex> clist;//создан пустой список
+    cout<<"Enter complex numbers.\n";
+    for (int i=0;i<w;i++){
+        cin>>r>>m;
+        t.constr(r,m);//заполнение его элементами
+
+        clist.push_back(t);
+        }
+    cout<<"List of complex numbers:\n";
+    for (complex n : clist){//вывод списка
+        cout << n.getre()<<"+"<<n.getim() << "i\n";}
+    return 0;
+}
